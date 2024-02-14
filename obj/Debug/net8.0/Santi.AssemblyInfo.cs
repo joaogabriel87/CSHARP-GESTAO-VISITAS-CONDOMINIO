8598d874147e64e2e4525b6ada99d45c915d297f
joaogabriel87 <joagabriel9@gmail.com>
2024-02-14 04:41:13 -0300

    git commit all

--- obj/Debug/net8.0/Santi.AssemblyInfo.cs
+++ obj/Debug/net8.0/Santi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Santi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44e30798c305637759946b51293ee40028e5eb24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98a84e01c956ad27ba65a3614a36dcb2cece8bce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Santi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Santi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
